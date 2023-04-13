32620188ffa9f6eb448a2e8acde67079b3f2aeae
Thekillergreece <95387832+Thekillergreece@users.noreply.github.com>
2023-04-13 17:22:29 +0000

    commit

---
+++ ArcheryTargetPractice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d7d7080c-090d-4b73-95b5-3a235d0abd47")]
+
+[assembly: AssemblyTitle("ArcheryTargetPractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArcheryTargetPractice")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: MelonInfo(typeof(ArcheryTargetPractice.Implementation), "ArcheryTargetPractice", "2.0.2", "Thekillergreece")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
